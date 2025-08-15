ac9e5af38354efcfe8b2311b87f128f29e7531b2
Tyrell Wellick <keshavniure99@gmail.com>
2025-08-15 01:55:15 -0800

    js added to login page

---
+++ script/boot.js
@@ -0,0 +1,13 @@
+const text = ["Establishing Connection...", 
+    "Connecting to tor network...",
+    "Connecting to hardware...",
+    "Encrypting data...",
+    "Routing traffic through Tor...",
+    "Securing device ip address...",
+    "Requesting connection...",
+    "Connecting to server...",
+    "Connection established.",
+    "Connection secured.",
+    "Connection ready.",
+    "All systems operational."
+];
--- script/login.js
+++ script/login.js
@@ -1,7 +1,7 @@
-const form = document.getElementById('login-form'); 
-const userName = document.getElementById('username');
-const password = document.getElementById('password'); 
-const loginMessage = document.getElementById('action-message');
+let form = document.getElementById('login-form'); 
+let userName = document.getElementById('username');
+let password = document.getElementById('password'); 
+let loginMessage = document.getElementById('action-message');
 
 
 const title = document.querySelector('#login-title');
@@ -15,3 +15,42 @@ if (i < text.length) {
 }
 }
 typeWriter()
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    loginMessage.textContent = '';
+    let user = userName.value.trim();
+    let pass = password.value.trim();
+   let valid = true;
+   if(!user){
+       loginMessage.textContent = 'Please enter your username.';
+       valid = false;
+   }
+   if(!pass){
+       loginMessage.textContent = 'Please enter your password.';
+       valid = false;
+   }if(!user && !pass){
+    loginMessage.textContent = 'Please enter username and password.';
+       valid = false;
+   }
+
+   if(valid){
+    if(user === "admin" && pass === "12345"){
+        console.log(user, pass);
+        
+        loginMessage.style.color = "#00ff88"
+        loginMessage.textContent = "You can entered into page"
+        setInterval(() =>{
+            window.location.href = 'boot.html'
+        })
+    }else{
+        loginMessage.textContent = "Sorry! UserName and password is incorrect"
+       document.querySelector('.login-container').classList.add('shake')
+       setInterval(() => {
+        document.querySelector('.login-container').classList.remove('shake')
+       }, 1200);
+    }
+   }
+   
+
+});
